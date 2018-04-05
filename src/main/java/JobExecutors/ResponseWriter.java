ffca11b7784938a8c74418f9ae5afbcdf3339c53
sudheer-g <bharadwaj.sudheer@gmail.com>
2018-04-05 10:29:48 +0530

    Initial Commit

---
+++ src/main/java/JobExecutors/ResponseWriter.java
@@ -0,0 +1,85 @@
+package JobExecutors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.AsyncContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.BufferOverflowException;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ResponseWriter implements Runnable {
+    private List<AsyncContext> jobs;
+    private Logger logger = LogManager.getLogger();
+
+
+    public ResponseWriter(List<AsyncContext> jobs) {
+        this.jobs = jobs;
+    }
+
+    private void writeToResponse(AsyncContext asyncContext) throws IOException, IllegalStateException {
+        int i;
+        int dataSize = 10485760;
+        try {
+            int written = asyncContext.getRequest().getAttribute("written") == null ? 0 : (int) asyncContext.getRequest()
+                    .getAttribute("written");
+            int limit = 102400;
+            PrintWriter out = asyncContext.getResponse().getWriter();
+            for (i = written + 1; i < dataSize; i++) {
+                if (i > 0 && i % limit == 0) {
+                    asyncContext.getRequest().setAttribute("written", i);
+                    break;
+                }
+                out.print((char) (ThreadLocalRandom.current().nextInt(90) + 32));
+            }
+            if (i >= dataSize) {
+                jobs.remove(asyncContext);
+                asyncContext.complete();
+            } else {
+                asyncContext.getRequest().setAttribute("writable", null);
+            }
+
+        } catch (BufferOverflowException e) {
+            e.printStackTrace();
+            jobs.remove(asyncContext);
+        }
+    }
+
+    private synchronized boolean isResponseWritable(AsyncContext asyncContext) {
+        if(asyncContext != null && asyncContext.getRequest().getAttribute("writable") == null) {
+            asyncContext.getRequest().setAttribute("writable",false );
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public void run() {
+        AsyncContext asyncContext = null;
+        while (true) {
+            if(Thread.interrupted()) {
+                logger.info("Hit thread exit");
+                break;
+            }
+            try {
+                int index = jobs.size() > 0 ? ThreadLocalRandom.current().nextInt(jobs.size()) : 0;
+                asyncContext = jobs.get(index);
+                if (isResponseWritable(asyncContext)) {
+                    writeToResponse(asyncContext);
+                }
+            }
+            catch (IndexOutOfBoundsException e) {
+            }
+            catch (IllegalStateException e) {
+                jobs.remove(asyncContext);
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/servlets/RandomResponseServlet.java
@@ -0,0 +1,55 @@
+package servlets;
+
+import JobExecutors.ResponseWriter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@WebServlet(name = "randRes", urlPatterns = "/random", asyncSupported = true)
+public class RandomResponseServlet extends HttpServlet {
+    private int requestHandleCapacity = 150;
+    private List<AsyncContext> jobs = Collections.synchronizedList(new ArrayList<>(requestHandleCapacity));
+    private Logger logger = LogManager.getLogger();
+    private int numberOfThreads = 1;
+    private Thread[] workers = new Thread[numberOfThreads];
+
+    @Override
+    public void init() throws ServletException {
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            workers[i] = new Thread(new ResponseWriter(jobs));
+            workers[i].setName("Response-Writer-Thread" + i);
+            workers[i].start();
+        }
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("Hit Random Write Servlet Destroy");
+        for (Thread thread : workers) {
+            thread.interrupt();
+            try {
+                thread.join(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        AsyncContext asyncContext = req.startAsync();
+        asyncContext.setTimeout(600000);
+        jobs.add(asyncContext);
+    }
+}
